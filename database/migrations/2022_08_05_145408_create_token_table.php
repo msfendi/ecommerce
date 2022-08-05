4ae396c95300f696d2566b0f28de567303852c08
msfendi <efendisyarifudin1@gmail.com>
2022-08-05 22:27:59 +0700

    first commit

---
+++ database/migrations/2022_08_05_145408_create_token_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTokenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('token', function (Blueprint $table) {
+            $table->bigIncrements('id_token');
+            $table->string('token_generate', 100);
+            $table->timestamp('expired_at');
+            $table->unsignedBigInteger('id_toko');
+        });
+
+        Schema::table('token', function (Blueprint $table) {
+            $table->foreign('id_toko')->references('id_toko')->on('toko');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('token');
+    }
+}
---
+++ database/migrations/2022_08_05_145654_create_toko_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTokoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('toko', function (Blueprint $table) {
+            $table->bigIncrements('id_toko');
+            $table->string('token_toko', 100);
+            $table->string('photo', 100);
+            $table->string('nama_toko', 200);
+            $table->text('alamat');
+            $table->json('kontak');
+            $table->enum('status', ['0', '1']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('toko');
+    }
+}
